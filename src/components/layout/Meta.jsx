51608566bec89d696b26f2c32394c4ce2fb055db
Paul Khoza <68129319+psk-98@users.noreply.github.com>
2023-02-10 13:37:44 +0200

    change SEO

--- src/components/layout/Meta.jsx
+++ src/components/layout/Meta.jsx
@@ -6,12 +6,12 @@ export default function Meta() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="keywords"
-        content="Paul Khoza, Web developer, Developer portfolio, Ecommerce developer, Web developer portfolio, Experienced full-stack developer, Pro Frontend, Backend, React, Django, Figma, Framer Motion, Web3, Ethers, Pretoria, Developer, Nextjs, Freelance"
+        content="Paul Khoza, Web developer, Developer portfolio, Ecommerce developer, Web developer portfolio, Experienced full-stack developer, Frontend, Backend, React, Django, Figma, Framer Motion, Web3, Ethers, Pretoria, Developer, Nextjs, Freelance"
       />
       <title>Paul Khoza | Portfolio</title>
       <meta
         name="description"
-        content="Fullstack django react developer with a bit of web3"
+        content="Expert React and Django web developer in Pretoria, South Africa. Offering full-stack web development services using the latest technologies and best practices. Contact me for your next project."
       />
       <link rel="icon" href="assets/media/favicon-32x32.png" />
       <link rel="icon" href="assets/media/favicon-16x16.png" />
