d6d743387e8aa050f1e476b382c1ae1f7d445980
Paul Khoza <68129319+psk-98@users.noreply.github.com>
2023-03-03 20:48:33 +0200

    add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,38 +5,24 @@ import Script from "next/script"
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script strategy="afterInteractive">
-        {`
-          (function(m, e, t, r, i, k, a) {
-            m[i] = m[i] || function() {
-              (m[i].a = m[i].a || []).push(arguments);
-            };
-            m[i].l = 1 * new Date();
-            k = e.createElement(t),
-            a = e.getElementsByTagName(t)[0],
-            k.async = 1,
-            k.src = r,
-            a.parentNode.insertBefore(k, a)
-          })
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-          ym(92683107, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-          });
-        `}
-      </Script>
-      <noscript>
-        <div>
-          <img
-            src="https://mc.yandex.ru/watch/92683107"
-            style={{ position: "absolute", left: "-9999px" }}
-            alt=""
-          />
-        </div>
-      </noscript>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-2VNZVQ54PG"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-2VNZVQ54PG', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
