d3f72cd8e7a87f2d4e4e297bc7cff194f21655bf
Paul Khoza <68129319+psk-98@users.noreply.github.com>
2023-03-04 22:06:06 +0200

    add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,16 +4,17 @@ import "@/styles/globals.css"
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS)}
       <script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-2VNZVQ54PG"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
       <script
         dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-2VNZVQ54PG',,{page_path: window.location.pathname,} );`,
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{page_path: window.location.pathname,} );`,
         }}
       />
       <Layout>
