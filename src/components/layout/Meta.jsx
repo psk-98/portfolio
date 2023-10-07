9c53d703ee7bb7a9d41939df441387188b47e307
psk-98 <paulk198@hotmail.com>
2023-10-07 09:55:05 +0200

    add faivons

--- src/components/layout/Meta.jsx
+++ src/components/layout/Meta.jsx
@@ -8,9 +8,24 @@ export default function Meta() {
         name="keywords"
         content="Paul Khoza, Technical Writer, Technical Writing, Web developer, Developer portfolio, Ecommerce developer, Web developer portfolio, Experienced full-stack developer, Frontend, Backend, React, Django, Figma, Framer Motion, Web3, Ethers, Pretoria, Developer, Nextjs, Freelance"
       />
-      <link rel="icon" href="assets/media/favicon-32x32.png" />
-      <link rel="icon" href="assets/media/favicon-16x16.png" />
-      <link rel="apple-touch-icon" href="assets/media/apple-touch-icon.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="assets/media/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="assets/media/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="assets/media/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   )
 }
